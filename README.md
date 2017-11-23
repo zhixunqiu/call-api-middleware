fc04788a7af3627ab4828f3f779cd9c08d74912d
wutaosusan <wutaosusan@didichuxing.com>
2017-11-23 19:54:03 +0800

    modify build

--- README.md
+++ README.md
@@ -4,9 +4,25 @@
 ```
 npm install --save call-api-middleware
 ```
- Then,to enable Redux MIddleware,use [`applyMiddleware()`](http://rackt.github.io/redux/docs/api/applyMiddleware.html):
+### Options
+  1.types:it can be a action type,or action create function.
 
-```js
+      requestType: before request,it is optional
+      successType: request success,it is necessary.
+      failureType: request fail,it is optional.
+  2.endPoint : request url
+
+  3.options : request option
+
+  4.checkStatusFn : A function to check your respond result,it is optional.
+
+  5.deaultMsg : A default message for error status.
+
+  6.failCallback : A function, you can deal the status of error responses,it is optional.
+### How to use
+  First,to enable Redux MIddleware,use [`applyMiddleware()`](http://rackt.github.io/redux/docs/api/applyMiddleware.html):
+
+```
 import { createStore, applyMiddleware } from 'redux';
 import apiMiddleware from 'call-api-middleware';
 import rootReducer from './reducers/index';
@@ -18,3 +34,18 @@ const createStoreWithMiddleware = applyMiddleware(
 
 const store = createStoreWithMiddleware(rootReducer);
 ```
+    Then,you can add network request in your action files like this:
+```
+export function rulelist(type){
+  let url = `${BASEURL}rule/rulelist`
+
+  return {
+    types: {successType:_rulelist,failureType:alertShow},
+    endPoint: url,
+    options: {credentials: 'include'},
+    checkStatusFn: checkStatus,
+    deaultMsg: "获取用车权限列表接口出错",
+  }
+}
+```
+
---
+++ es/index.js
@@ -0,0 +1,91 @@
+import fetch from 'isomorphic-fetch';
+
+var parseResponse = function parseResponse(response) {
+  var contentType = response.headers.get('Content-Type') || '';
+
+  if (contentType.indexOf('json') !== -1) {
+    return response.json();
+  } else if (contentType.indexOf('text') !== -1) {
+    return response.text();
+  }
+
+  return response;
+};
+
+var actionWith = function actionWith(actionType, payload) {
+  var nextAction = void 0;
+  if (typeof actionType === 'function') {
+    nextAction = actionType(payload);
+  } else {
+    // convert strings or symbols to FSA object
+    if (typeof actionType === 'string' || typeof actionType === 'symbol') {
+      nextAction = { type: actionType };
+    } else {
+      nextAction = actionType;
+    }
+
+    if (payload) {
+      nextAction.payload = payload;
+    }
+  }
+  if (payload instanceof Error) {
+    nextAction.error = true;
+  }
+
+  return nextAction;
+};
+
+export function apiMiddleware(_ref) {
+  var dispatch = _ref.dispatch,
+      getState = _ref.getState;
+
+  return function (next) {
+    return function (action) {
+      var types = action.types,
+          endPoint = action.endPoint,
+          options = action.options,
+          _action$checkStatusFn = action.checkStatusFn,
+          checkStatusFn = _action$checkStatusFn === undefined ? function () {} : _action$checkStatusFn,
+          _action$deaultMsg = action.deaultMsg,
+          deaultMsg = _action$deaultMsg === undefined ? '' : _action$deaultMsg,
+          _action$failCallback = action.failCallback,
+          failCallback = _action$failCallback === undefined ? null : _action$failCallback;
+
+
+      if (!types) {
+        return typeof action === 'function' ? action(dispatch, getState) : next(action);
+      }
+
+      if (!endPoint) {
+        throw new Error('Expected endPoint to be a url.');
+      }
+
+      var requestType = types.requestType,
+          successType = types.successType,
+          failureType = types.failureType;
+
+
+      !!requestType && next(actionWith(requestType));
+
+      return fetch(endPoint, options).then(parseResponse).then(checkStatusFn).then(function (response) {
+        return next(actionWith(successType, response));
+      }, function (err) {
+        if (failureType) {
+          var msg = err.response && err.response.errmsg || deaultMsg;
+          return next(actionWith(failureType, { msg: msg, type: 'warning' }));
+        }
+        if (failCallback) {
+          failCallback(next, err, deaultMsg);
+        }
+      })['catch'](function (err) {
+        var msg = String(err);
+        if (failureType) {
+          return next(actionWith(failureType, { msg: msg, type: 'warning' }));
+        }
+        if (failCallback) {
+          failCallback(next, err, deaultMsg);
+        }
+      });
+    };
+  };
+}
